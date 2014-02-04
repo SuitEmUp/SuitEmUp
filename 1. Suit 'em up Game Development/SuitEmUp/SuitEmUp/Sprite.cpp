48590190a16dc2a97d6fccc883810a4f64202d09
Labraoban <labanmelander@gmail.com>
2014-02-04 16:40:21 +0100

    Sprite.h/cpp

---
+++ 1. Suit 'em up Game Development/SuitEmUp/SuitEmUp/Sprite.cpp
@@ -0,0 +1,14 @@
+#include "Sprite.h"
+
+Sprite::Sprite()
+{
+
+};
+
+void Sprite::CreateSprite(std::string filename, int p_x, int p_y, int p_width, int p_height)
+{
+	m_x = p_x;
+	m_y = p_y;
+	m_width = p_width;
+	m_height = m_height;
+};
--- 1. Suit 'em up Game Development/SuitEmUp/SuitEmUp/Sprite.h
+++ 1. Suit 'em up Game Development/SuitEmUp/SuitEmUp/Sprite.h
@@ -1,10 +1,22 @@
 #pragma once
 
+#include <SFML\Graphics.hpp>
+#include <SFML\Window.hpp>
 
-class Sprite
+#include <string>
+
+class Sprite : sf::Sprite
 {
 public:
 	Sprite();
 
+	void CreateSprite(std::string filename, int x, int y, int width, int height);
 
+	virtual void Update(float deltatime){};
+
+protected:
+	int m_x;
+	int m_y;
+	int m_width;
+	int m_height;
 };
