20e83bc03cd39877c8a65645ac058f185d078bb7
Knifedwarf <viktor.myhrberg@hotmail.se>
2014-02-24 15:36:44 +0100

    NO MORE BUUUUUUUUUUGS

--- 1. Suit 'em up Game Development/SuitEmUp/SuitEmUp/GameObjectManager.cpp
+++ 1. Suit 'em up Game Development/SuitEmUp/SuitEmUp/GameObjectManager.cpp
@@ -209,7 +209,7 @@ void GameObjectManager::Update(float deltatime)
 	for(int i = 0; i< m_enemy_projectiles.size(); i++){ 
 		//Updates all enemy projectiles. The return true if they collide with the truck. The truck is also damaged.
 		if(m_enemy_projectiles.at(i)->Update(m_truck, deltatime)){
-			delete m_enemy_projectiles.at(i)->GetSprite();
+			//delete m_enemy_projectiles.at(i)->GetSprite();
 			delete m_enemy_projectiles[i];
 			m_enemy_projectiles.erase(m_enemy_projectiles.begin()+i);
 			i--;
@@ -219,7 +219,7 @@ void GameObjectManager::Update(float deltatime)
 
 	for(int i = 0; i< m_player_projectiles.size(); i++){
 		if(m_player_projectiles.at(i)->Update(m_truck, deltatime)){
-			delete m_player_projectiles.at(i)->GetSprite();
+			//delete m_player_projectiles.at(i)->GetSprite();
 			delete m_player_projectiles[i];
 			m_player_projectiles.erase(m_player_projectiles.begin()+i);
 			i--;
@@ -233,13 +233,13 @@ void GameObjectManager::Update(float deltatime)
 			if(m_spawner->EnemyDestroyer(m_enemies.at(j), m_player_projectiles.at(i))){
 				//delete (*it)->GetSprite();
 				//delete (*at)->GetSprite();
-				delete m_player_projectiles.at(i)->GetSprite();
+				//delete m_player_projectiles.at(i)->GetSprite();
 				delete m_player_projectiles[i];
 				m_player_projectiles.erase(m_player_projectiles.begin()+i);
 				if(m_enemies.at(j)->Damaged(m_player->GetDamage())<=0){
 
 
-					delete m_enemies.at(j)->GetSprite();
+				//	delete m_enemies.at(j)->GetSprite();
 
 					int chance = rand()%20;
 					if(chance == 15)
@@ -275,7 +275,7 @@ void GameObjectManager::Update(float deltatime)
 						m_vRepairKits.push_back(new RepairKit(m_supers.at(j)->GetPosition(), m_supers.at(j)->GetVelocity(), 
 							m_spritemanager->Load("../data/sprites/ToolBox.png", "Toolbox", 1, 1)));
 					}
-					delete m_supers.at(j)->GetSprite();
+				//	delete m_supers.at(j)->GetSprite();
 					delete m_supers[j];
 					m_supers.erase(m_supers.begin()+j);
 
@@ -302,7 +302,7 @@ void GameObjectManager::Update(float deltatime)
 				if(m_girls.at(j)->Damaged(m_player->GetDamage())<=0){
 					m_vRepairKits.push_back(new RepairKit(m_girls.at(j)->GetPosition(), m_girls.at(j)->GetVelocity(), 
 						m_spritemanager->Load("../data/sprites/ToolBox.png", "Toolbox", 1, 1)));
-					delete m_girls.at(j)->GetSprite();
+				//	delete m_girls.at(j)->GetSprite();
 					delete m_girls[j];
 					m_girls.erase(m_girls.begin()+j);
 					//SCORE COUNT
@@ -320,7 +320,7 @@ void GameObjectManager::Update(float deltatime)
 		if(m_vRepairKits.at(i)->Update(m_truck, m_player, deltatime)){
 			//delete (*it)->GetSprite();
 			m_truck->Healed();
-			delete m_vRepairKits.at(i)->GetSprite();
+		//	delete m_vRepairKits.at(i)->GetSprite();
 			delete m_vRepairKits[i];
 			m_vRepairKits.erase(m_vRepairKits.begin()+i);
 			i--;
@@ -558,9 +558,40 @@ void GameObjectManager::CreateTrinketButtons()
 
 void GameObjectManager::DrawTrinketButtons()
 {
-		for(int i=0; i<m_vTrinketButtons.size(); i++){
+	for(int i=0; i<m_vTrinketButtons.size(); i++){
 		if(m_vTrinketButtons.at(i)!=nullptr){
 			m_window->draw(*m_vTrinketButtons.at(i)->GetSprite()); // draws all buttons
 		}
 	}
+
+};
+
+void GameObjectManager::EraseButtons(){
+	for(int i = 0; i< m_vButtons.size(); i++){
+		//delete m_vButtons.at(i)->GetSprite();
+		delete m_vButtons[i];
+		m_vButtons.erase(m_vButtons.begin()+i);
+		i--;
+	};
+	m_vButtons.clear();
+};
+
+void GameObjectManager::EraseCustomizationButtons(){
+	for(int i = 0; i < m_vCustomizeButtons.size(); i++){
+	//	delete m_vCustomizeButtons.at(i)->GetSprite();
+		delete m_vCustomizeButtons[i];
+		m_vCustomizeButtons.erase(m_vCustomizeButtons.begin()+i);
+		i--;
+	};
+	m_vCustomizeButtons.clear();
+};
+
+void GameObjectManager::EraseTrinketButtons(){
+	for(int i = 0; i < m_vTrinketButtons.size(); i++){
+	//	delete m_vTrinketButtons.at(i)->GetSprite();
+		delete m_vTrinketButtons[i];
+		m_vTrinketButtons.erase(m_vTrinketButtons.begin()+i);
+		i--;
+	};
+	m_vTrinketButtons.clear();
 };
